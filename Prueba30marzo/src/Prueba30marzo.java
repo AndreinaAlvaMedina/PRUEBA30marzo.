7af8ee475d754ecc1c4fa8dfa040289e2999eac6
Jocsan Lopez <yocsanyl77@gmail.com>
2020-04-02 18:22:23 -0600

    Update Prueba30marzo.java

--- Prueba30marzo/src/Prueba30marzo.java
+++ Prueba30marzo/src/Prueba30marzo.java
@@ -15,22 +15,27 @@ public class Prueba30marzo {
         System.out.println("Andreina Alva");
         System.out.println("Zona 3, Huehuetenango");
         System.out.println("41807261");
+        System.out.println();
      
         System.out.println("Daniel Alonzo Herrera ");
         System.out.println("7a. AVE 9-81 Zona 5, Huehuetenango, Huehuetenango ");
         System.out.println("3059-6671 ");
-        
+        System.out.println();
+     
         System.out.println("Carlos Andrés Miranda Méndez");
         System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
         System.out.println("58385781");
+        System.out.println();
      
         System.out.println("JAN CARLO DE LEON CIFUENTES");
         System.out.println("11  CALLE 2-43 ZONA 5 COLONIA LOS ENCINOS");
         System.out.println("33361433");
+        System.out.println();
      
         System.out.println("Denis Joel Cifuentes Cano");
         System.out.println("Zona 1 Canton cementerio Chiantla");
         System.out.println("45883960");
+        System.out.println();
      
         System.out.println("Jocsan Abisaí López González");
         System.out.println("Zona 11, Huehuetenango, Huehuetenango");
